19170c7d75e21dda559b6b9fa1a2cf72c5c0f3bf
Douglas Pimentel Teixeira <douglaspt@gmail.com>
2024-06-06 11:59:27 -0300

    Consulta simples com query methods

--- src/main/java/com/dpt/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/dpt/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.dpt.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,9 @@ import com.dpt.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String title);
+		
 
 
 }
