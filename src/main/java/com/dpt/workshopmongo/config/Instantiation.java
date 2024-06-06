d9a7b946d14179e4f9a6a3a6664e36f090418b89
Douglas Pimentel Teixeira <douglaspt@gmail.com>
2024-06-06 14:48:18 -0300

    Consulta com vários critérios

--- src/main/java/com/dpt/workshopmongo/config/Instantiation.java
+++ src/main/java/com/dpt/workshopmongo/config/Instantiation.java
@@ -43,9 +43,9 @@ public class Instantiation implements CommandLineRunner{
 		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("29/03/2018"), new AuthorDTO(alex));
-		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("02/04/2018"), new AuthorDTO(bob));
-		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("02/04/2018"), new AuthorDTO(alex));
+		CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 		
 		p1.getCommments().addAll(Arrays.asList(c1, c2));
 		p2.getCommments().addAll(Arrays.asList(c3));
